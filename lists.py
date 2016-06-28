4fe424c0d76b7dd55402a9250ea428be530dad8f
JerubaalDunelm <JerubaalDunelm@users.noreply.github.com>
2016-06-28 15:04:47 +0100

    two ways of looping through array

--- lists.py
+++ lists.py
@@ -1,5 +1,10 @@
 attendees = ["Gids","Lee","Helen","Erna"]
 # zero-based, so attendees[1] prints "Lee"
 
+#loop through position
 for i in range(0,len(attendees)):
     print attendees[i]
+
+#loop through values    
+for a in attendees:
+    print a
