2656308f29dd03e3da13fda1967ae3f7a273c08c
JerubaalDunelm <JerubaalDunelm@users.noreply.github.com>
2016-06-28 14:14:16 +0100

    added for loop and cmd variables

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,10 @@
-print "Hello World"
+import sys
+
+personName = sys.argv[1]
+attribute = sys.argv[2]
+
+print "Hello " + personName + ", you are " + attribute + "!"
+
+timesTable = int(sys.argv[3])
+for i in range(1,20):
+	print timesTable*i
